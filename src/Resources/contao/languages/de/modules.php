13ed927b2bb01206adc83705d88809575996f073
jonnysp <jonnyspitzner@googlemail.com>
2022-09-08 09:53:38 +0200

    -

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['MOD']['animal'] = array('Tiere Verwaltung');
-$GLOBALS['TL_LANG']['MOD']['animalcategorie'] = array('Tiere','Verwalte die Tiere');
+$GLOBALS['TL_LANG']['MOD']['animals'] = array('Tiere Verwaltung');
+$GLOBALS['TL_LANG']['MOD']['animalscategorie'] = array('Tiere','Verwalte die Tiere');
 
-$GLOBALS['TL_LANG']['CTE']['animalcategorie_viewer'] = array("Tier-Kategorie","");
+$GLOBALS['TL_LANG']['CTE']['animalscategorie_viewer'] = array("Tier-Kategorie","");
