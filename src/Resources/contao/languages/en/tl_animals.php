faadfbd2d48e073bba837a4997ad9c0c90fd315a
jonnysp <jonnyspitzner@googlemail.com>
2022-09-08 09:51:43 +0200

    -

--- src/Resources/contao/languages/en/tl_animals.php
+++ src/Resources/contao/languages/en/tl_animals.php
@@ -3,33 +3,33 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_recipes']['title']       = array('Recipe name', 'Enter the recipe name here.');
-$GLOBALS['TL_LANG']['tl_recipes']['image']       = array('Recipe picture', 'Recipe picture.');
-$GLOBALS['TL_LANG']['tl_recipes']['description'] = array('description', 'Recipe description.');
-$GLOBALS['TL_LANG']['tl_recipes']['ingredients'] = array('ingredients', 'Recipe ingredients.');
-$GLOBALS['TL_LANG']['tl_recipes']['preparation'] = array('preparation', 'Recipe preparation.');
-$GLOBALS['TL_LANG']['tl_recipes']['toggle'] 	 = array('ON/OFF','On / Off');
+$GLOBALS['TL_LANG']['tl_animals']['title']       = array('Animal name', 'Enter the recipe name here.');
+$GLOBALS['TL_LANG']['tl_animals']['image']       = array('Animal picture', 'Animal picture.');
+$GLOBALS['TL_LANG']['tl_animals']['description'] = array('description', 'Animal description.');
+$GLOBALS['TL_LANG']['tl_animals']['ingredients'] = array('ingredients', 'Animal ingredients.');
+$GLOBALS['TL_LANG']['tl_animals']['preparation'] = array('preparation', 'Animal preparation.');
+$GLOBALS['TL_LANG']['tl_animals']['toggle'] 	 = array('ON/OFF','On / Off');
 
-$GLOBALS['TL_LANG']['tl_recipes']['images'] 	 = array('More recipe pictures','select more recipe images here.');
-$GLOBALS['TL_LANG']['tl_recipes']['imagessort']  = array('image sorting', '');
+$GLOBALS['TL_LANG']['tl_animals']['images'] 	 = array('More recipe pictures','select more recipe images here.');
+$GLOBALS['TL_LANG']['tl_animals']['imagessort']  = array('image sorting', '');
 
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_recipes']['title_legend'] = 'Recipe';
-$GLOBALS['TL_LANG']['tl_recipes']['title_legends'] = 'Recipes';
+$GLOBALS['TL_LANG']['tl_animals']['title_legend'] = 'Animal';
+$GLOBALS['TL_LANG']['tl_animals']['title_legends'] = 'Animals';
 
-$GLOBALS['TL_LANG']['tl_recipes']['title_images'] = 'Recipe picture';
-$GLOBALS['TL_LANG']['tl_recipes']['title_description'] = 'Recipe descriptions';
+$GLOBALS['TL_LANG']['tl_animals']['title_images'] = 'Animal picture';
+$GLOBALS['TL_LANG']['tl_animals']['title_description'] = 'Animal descriptions';
 
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_recipes']['new']        = array('New Recipe', 'Create a new recipe');
-$GLOBALS['TL_LANG']['tl_recipes']['show']       = array('Recipe details', 'Details of the recipe ID %s');
-$GLOBALS['TL_LANG']['tl_recipes']['edit']       = array('Recipe Edit', 'Edit recipe ID %s');
-$GLOBALS['TL_LANG']['tl_recipes']['copy']       = array('Recipe Copy', 'Copy recipe ID %s');
-$GLOBALS['TL_LANG']['tl_recipes']['delete']     = array('Recipe Delete','Delete recipe ID %s');
-$GLOBALS['TL_LANG']['tl_recipes']['cut']    	= array('Recipe Cut', 'Cut recipe ID %s');
+$GLOBALS['TL_LANG']['tl_animals']['new']        = array('New Animal', 'Create a new recipe');
+$GLOBALS['TL_LANG']['tl_animals']['show']       = array('Animal details', 'Details of the recipe ID %s');
+$GLOBALS['TL_LANG']['tl_animals']['edit']       = array('Animal Edit', 'Edit recipe ID %s');
+$GLOBALS['TL_LANG']['tl_animals']['copy']       = array('Animal Copy', 'Copy recipe ID %s');
+$GLOBALS['TL_LANG']['tl_animals']['delete']     = array('Animal Delete','Delete recipe ID %s');
+$GLOBALS['TL_LANG']['tl_animals']['cut']    	= array('Animal Cut', 'Cut recipe ID %s');
